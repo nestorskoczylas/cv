d8a30c5fe7075e518711dce29dc2aeffe22c94e5
nestorskoczylas <nestor.skoczylas23@gmail.com>
2025-01-13 13:11:53 +0100

    feat: create user profile card with image, name, separator, and job title

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -26,10 +26,31 @@
     </q-header>
 
     <q-page-container class="background-container">
-      <q-page class="q-pa-md">
-        <div class="scroll-content">
-          <h1>Bienvenue</h1>
-        </div>
+      <q-page class="q-pa-md flex flex-center scroll-content">
+        <q-card style="box-shadow: 0 4px 12px rgba(0, 0, 0, 0.1)">
+          <q-card-section class="q-pa-lg background-card-section text-center">
+            <div class="image-container">
+              <img
+                src="/public/icons/favicon-425x425.png"
+                alt="me"
+                :width="200"
+                :height="200"
+                class="profile-image"
+              />
+            </div>
+            <div class="card-name">
+              <p class="text-bold">Nestor</p>
+              <p class="text-bold">Skoczylas</p>
+            </div>
+            <q-separator
+              horizontal
+              color="primary"
+              size="0.2rem"
+              class="separator"
+            />
+            <span class="profession">Développeur C# .NET VueJS</span>
+          </q-card-section>
+        </q-card>
       </q-page>
     </q-page-container>
 
@@ -65,7 +86,7 @@
             square
             icon="mdi-github"
             @click="openLink('https://github.com/nestorskoczylas')"
-            class="footer-icon small"
+            class="footer-icon"
           />
         </div>
       </q-toolbar>
@@ -159,6 +180,34 @@ const openLink = (url: string) => {
 .scroll-content {
   max-height: 100%;
   overflow-y: auto;
+}
+
+.background-card-section {
+  background: $secondary;
+}
+
+.profile-image {
+  border-radius: 50%;
+  object-fit: cover;
+  background-color: $white;
+}
+
+.text-center .card-name {
+  margin: 1.5rem 0;
+}
+
+.card-name .text-bold {
+  margin: 0;
+  font-size: 1.75rem;
+}
+
+.separator {
+  margin-bottom: 2rem;
+}
+
+.text-center span.profession {
+  margin-top: 0.5rem;
+  letter-spacing: 0.1rem;
 }
 
 /* Footer */
