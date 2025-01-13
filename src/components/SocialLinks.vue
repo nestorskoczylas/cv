29daaf8b3c2b5355f72c5d8de3b8f31c7ca5592f
nestorskoczylas <nestor.skoczylas23@gmail.com>
2025-01-13 16:05:44 +0100

    refactor: Improve variable and class naming in SocialLinks component

--- src/components/SocialLinks.vue
+++ src/components/SocialLinks.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="social-links">
+  <div class="social-links__container">
     <q-btn
-      v-for="link in socialLinks"
+      v-for="link in socialLinksList"
       :key="link.icon"
       flat
       square
       :icon="link.icon"
-      @click="openLink(link.url)"
-      class="icon"
+      @click="openExternalLink(link.url)"
+      class="social-links__icon"
     />
   </div>
 </template>
@@ -15,24 +15,24 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 
-const socialLinks = ref([
+const socialLinksList = ref([
   { icon: 'mdi-linkedin', url: 'https://www.linkedin.com/in/nestorskoczylas' },
   { icon: 'mdi-github', url: 'https://github.com/nestorskoczylas' },
 ]);
 
-const openLink = (url: string) => {
+const openExternalLink = (url: string) => {
   window.open(url, '_blank');
 };
 </script>
 
 <style lang="scss" scoped>
-.icon {
+.social-links__icon {
   color: $dark;
   font-size: 1.2rem;
   margin-left: 8px;
 }
 
-.social-links {
+.social-links__container {
   display: flex;
   align-items: center;
 }
