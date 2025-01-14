a2cdd8a39f2f13785c69da433588e5e2695a60ca
Nestor Skoczylas <33962393+nestorskoczylas@users.noreply.github.com>
2025-01-14 16:29:59 +0100

    Add Resume Page (#10)
    * Experience section (#5)
    
    * feat: Set up the basic structure for the 'Experience' section page
    
    * feat: Add the experience cards and bind data
    
    * feat: Style the experience section layout
    
    * feat: add style for skill chips
    
    * refactor: CSS with BEM naming convention for 'Experience' section
    
    * feat: add new professional experiences to the 'experiences' data
    
    * feat: add new experience as 'Pyrotechnicien' to 'experiences' data
    
    * Diplomes section (#6)
    
    * feat: Add 'Diplomas' section with educational details
    
    * refactor: rename same css between 'Experiences' and 'Diplomas'
    
    * feat: Add link to diplomas
    
    * feat: Add new 'Formations' section (#7)
    
    * Competences et qualities sections (#8)
    
    * feat: add 'Competences' section
    
    * refactor: rename css same used
    
    * feat: Remove margin-top for 'Langages de programmation' section
    
    * feat: add 'Qualités' section
    
    * feat: add 'Expériences personnelles' section (#9)
    
    * refactor: create ResumeSection, ResumeCard, and ResumeChipList components with slots
    
    * refactor: implement types to organize resume sections
    
    * fix: remove unused <div>
    
    * chore: update version to 0.4.4

---
+++ src/components/ResumeCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-card class="resume__card q-mb-md" bordered shadow="2">
+    <q-card-section>
+      <slot name="header"></slot>
+      <slot name="content"></slot>
+      <slot name="footer"></slot>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped>
+.resume__card {
+  width: 100%;
+  max-width: 800px;
+  margin-bottom: 20px;
+  padding: 20px;
+  border-radius: 8px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+</style>
---
+++ src/components/ResumeChipList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="resume__skills">
+    <strong>{{ label }} :</strong>
+    <div class="resume__skills-list">
+      <q-chip v-for="(item, index) in items" :key="index" class="q-mr-sm">
+        {{ item }}
+      </q-chip>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  items: {
+    type: Array,
+    required: true,
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.resume__skills {
+  font-size: 14px;
+}
+
+.resume__skills-list {
+  margin-top: 10px;
+}
+
+.q-chip {
+  margin: 5px;
+  background-color: transparent;
+  border: 1px solid $primary;
+}
+</style>
