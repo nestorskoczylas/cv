967cb6630f202ca653028be5dfbd92b70942f0e4
nestorskoczylas <nestor.skoczylas23@gmail.com>
2025-01-15 10:10:39 +0100

    feat: add Experience page

---
+++ src/pages/Experience.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="background-container">
+    <q-page class="q-pa-md flex flex-center scroll-content borders">
+      <p>Experience</p>
+    </q-page>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+.background-container {
+  background: $background;
+}
+
+.scroll-content {
+  max-height: 100%;
+  overflow-y: auto;
+}
+
+.borders {
+  border: 1px solid $background;
+}
+</style>
