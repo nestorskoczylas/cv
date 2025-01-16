fb9db9c0ef8e1dbc90f7c4f0fec0a3ac802fe4a1
nestorskoczylas <nestor.skoczylas23@gmail.com>
2025-01-16 15:16:44 +0100

    init gh-pages

--- src/utils/navigation.ts
+++
@@ -1,9 +0,0 @@
-import type { Router } from 'vue-router';
-
-export const navigateTo = (router: Router, route: string) => {
-  router.push({ name: route });
-};
-
-export const openExternalLink = (url: string) => {
-  window.open(url, '_blank');
-};
