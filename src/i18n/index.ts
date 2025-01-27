2361f1a5b8dfb976248eaff3dfb655e3704faca8
nestorskoczylas <nestor.skoczylas23@gmail.com>
2025-01-27 12:44:09 +0100

    refactor: i18n translation management

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,26 +1,5 @@
 import { createI18n } from 'vue-i18n'
-
-import constants from './constants'
-import fr_Constants from './fr/constants'
-import fr_BioCard from './fr/components/aboutMe/BioCard'
-import fr_TitledContent from './fr/components/common/TitledContent'
-import fr_Experience from './fr/pages/Experience'
-import fr_Projects from './fr/pages/Projects'
-import fr_Resume from './fr/pages/Resume'
-
-const messages = {
-  fr: {
-    untranslatable: constants,
-    constants: fr_Constants,
-    bioCard: fr_BioCard,
-    titledContent: fr_TitledContent,
-    pages: {
-      experience: fr_Experience,
-      projects: fr_Projects,
-      resume: fr_Resume,
-    },
-  },
-}
+import messages from './messages'
 
 const i18n = createI18n({
   locale: 'fr',
---
+++ src/i18n/locales/en/constants.ts
@@ -0,0 +1,6 @@
+export default {
+  profession: 'C# .NET Vue.js Developer',
+  aboutMe: 'About Me',
+  resume: 'Resume',
+  projects: 'Projects',
+}
