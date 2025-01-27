29ec77600f10bea2d6eb40f174ebc72a525c0cbb
nestorskoczylas <nestor.skoczylas23@gmail.com>
2025-01-27 12:17:57 +0100

    refactor: load projects dynamically with i18n

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -2,7 +2,7 @@
   <div class="background">
     <q-page class="q-pa-md flex flex-center borders">
       <div class="project__section">
-        <TitledContent title="Projets personnels">
+        <TitledContent :title="$t('pages.projects.title')">
           <ContentCard
             v-for="(item, index) in projects"
             :key="index"
@@ -20,56 +20,15 @@
 </template>
 
 <script lang="ts" setup>
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
 import TitledContent from '@/components/common/TitledContent.vue'
 import ContentCard from '@/components/common/ContentCard.vue'
-import { ref } from 'vue'
+import type { Project } from '@/types/project'
 
-const projects = ref([
-  {
-    title: 'Application CV en VueJS',
-    period: '2024',
-    description:
-      'Ce projet a pour objectif de présenter mon CV sous forme d’application web. Le projet est développé en VueJS avec le framework Quasar et l’implémentation VueJS Options API. Il permet de mettre en avant mes compétences, mes expériences professionnelles et mes réalisations de manière interactive et dynamique.',
-    skillTitle: 'Technologies utilisées',
-    skills: ['VueJS', 'Quasar', 'TypeScript', 'Vue Router', 'Vuex', 'Vue-i18n', 'GitHub Pages'],
-    links: [
-      {
-        label: 'GitHub : My CV App',
-        url: 'https://github.com/nestorskoczylas/my-cv-app',
-      },
-      {
-        label: 'Démo : My CV App',
-        url: 'https://nestorskoczylas.github.io/my-cv-app/#/aboutme',
-      },
-    ],
-  },
-  {
-    title: 'Projet de fin d’études : Stud’Hunt',
-    period: 'Septembre 2023 - Janvier 2024',
-    description:
-      'Stud’Hunt est un projet de fin d’études réalisé en équipe dans le cadre de mon Master Informatique, en collaboration avec l’IAE et son Master MCC. Il s’agit d’une application web de recherche de stages et d’alternances, qui met en relation les étudiants et les entreprises.',
-    skillTitle: 'Technologies utilisées',
-    skills: [
-      'Flutter',
-      'Spring Boot',
-      'Spring Security',
-      'BLoC',
-      'GitHub Actions',
-      'Docker',
-      'Google Compute Engine',
-    ],
-    links: [
-      {
-        label: 'GitHub : Stud’Hunt',
-        url: 'https://github.com/Stud-Hunt',
-      },
-      {
-        label: 'LinkedIn : Stud’Hunt',
-        url: 'https://www.linkedin.com/company/stud-hunt/',
-      },
-    ],
-  },
-])
+const { tm } = useI18n()
+
+const projects = computed(() => tm('pages.projects.projects') as Project[])
 </script>
 
 <style lang="scss" scoped>
---
+++ src/types/project.d.ts
@@ -0,0 +1,11 @@
+export type Project = {
+  title: string;
+  period: string;
+  description: string;
+  skillTitle: string;
+  skills: string[];
+  links: {
+    label: string;
+    url: string;
+  }[];
+}
