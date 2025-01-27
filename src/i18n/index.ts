aa0dc30807b4e9cab6842723ae634fc2c9da7241
nestorskoczylas <nestor.skoczylas23@gmail.com>
2025-01-27 10:50:32 +0100

    feat: set up Vue I18n for internationalization

---
+++ src/i18n/index.ts
@@ -0,0 +1,13 @@
+import { createI18n } from 'vue-i18n'
+
+const messages = {
+  // fr
+}
+
+const i18n = createI18n({
+  locale: 'fr',
+  fallbackLocale: 'fr',
+  messages
+});
+
+export default i18n;
