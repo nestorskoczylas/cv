0d14a87a88ff80c6fda6f995d5e9bc401b6d3d57
nestorskoczylas <nestor.skoczylas23@gmail.com>
2025-01-27 11:11:27 +0100

    feat: create individual translation files for each component

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,7 +1,29 @@
 import { createI18n } from 'vue-i18n'
 
+import fr_AboutMeCard from './fr/components/aboutMe/AboutMeCard'
+import fr_BioCard from './fr/components/aboutMe/BioCard'
+import fr_TitledContent from './fr/components/common/TitledContent'
+import fr_AppFooter from './fr/layouts/AppFooter'
+import fr_AppHeader from './fr/layouts/AppHeader'
+import fr_Experience from './fr/pages/Experience'
+import fr_Projects from './fr/pages/Projects'
+import fr_Resume from './fr/pages/Resume'
+
 const messages = {
-  // fr
+  fr: {
+    aboutMe: fr_AboutMeCard,
+    bioCard: fr_BioCard,
+    titledContent: fr_TitledContent,
+    layouts: {
+      appFooter: fr_AppFooter,
+      appHeader: fr_AppHeader
+    },
+    pages:  {
+      experience: fr_Experience,
+      projects: fr_Projects,
+      resume: fr_Resume
+    }
+  }
 }
 
 const i18n = createI18n({
