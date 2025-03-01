54fa5ab0c6e5a536ce76c11cef584a6348c74ee3
nestorskoczylas <nestor.skoczylas23@gmail.com>
2025-03-01 14:29:57 +0100

    fix: missins element

--- src/i18n/locales/en.ts
+++ src/i18n/locales/en.ts
@@ -1,6 +1,7 @@
 import constants from './en/constants'
 import BioCard from './en/components/aboutMe/BioCard'
 import TitledContent from './en/components/common/TitledContent'
+import ContentCard from './en/components/common/ContentCard'
 import Experience from './en/pages/Experience'
 import Projects from './en/pages/Projects'
 import Resume from './en/pages/Resume'
@@ -9,6 +10,7 @@ export default {
   constants,
   bioCard: BioCard,
   titledContent: TitledContent,
+  contentCard: ContentCard,
   pages: {
     experience: Experience,
     projects: Projects,
